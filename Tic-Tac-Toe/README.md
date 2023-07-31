ce7e6ae311029287fb85fbc51b6aa8e6909b1092
Meowmaster <ashenoy64@gmail.com>
2023-07-31 12:10:59 +0530

    Added readme to TicTacToe and made some changes to input

---
+++ Tic-Tac-Toe/README.md
@@ -0,0 +1,41 @@
+# Tic Tac Toe
+
+Welcome to Tic Tac Toe - a simple terminal-based implementation of the classic Tic Tac Toe game!
+
+## How to Play
+
+1. Run the executable file:
+
+   ```bash
+   ./TicTacToe
+   ```
+
+2. The game board is a 3x3 grid represented by numbers from 1 to 9:
+
+   ```
+   1 | 2 | 3
+   ---------
+   4 | 5 | 6
+   ---------
+   7 | 8 | 9
+   ```
+
+3. Players take turns to place their markers ('X' or 'O') on the board by choosing a number.
+
+4. The objective is to get three of your markers in a row (horizontally, vertically, or diagonally) before your opponent.
+
+5. The game ends when either one player wins or the board is full with no winner.
+
+6. To exit the game at any time, press `Ctrl + C`.
+
+## How to Build (Optional)
+
+If you want to compile the source code yourself, follow these steps:
+
+1. Compile the source code using `g++`.
+   ```bash
+   g++ main.cpp -o TicTacToe
+   ```
+
+Thank you for playing Tic Tac Toe! Enjoy the challenge of outwitting your opponent and getting three in a row. If you have any questions or feedback, feel free to reach out. Have fun playing! 🎲
+
--- Tic-Tac-Toe/main.cpp
+++ Tic-Tac-Toe/main.cpp
@@ -1,107 +1,135 @@
-#include<iostream>
-#include<stdlib.h>
-#include<conio.h>
-#include<bits/stdc++.h>
-
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <cstdlib>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
-class Game{
-    private:
+class Game
+{
+private:
     int arr[3][3];
 
-    public:
+public:
     Game()
     {
-        for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
-        arr[i][j]=0;
-        bool gameOver=false;
-        bool gameWon=false;
-        int who=-1;
-        printBoard();
-        while(!gameOver)
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                arr[i][j] = 0;
+        bool gameOver = false;
+        bool gameWon = false;
+        int who = -1;
+        // printBoard();
+        while (!gameOver)
         {
-            
+
             player1Input();
-            if(checkWin(1))
+            if (checkWin(1))
             {
-                gameOver=true;
-                gameWon=true;
-                who=1;
+                gameOver = true;
+                gameWon = true;
+                who = 1;
                 continue;
             }
             player2Input();
-            if(checkWin(2))
+            if (checkWin(2))
             {
-                gameOver=true;
-                gameWon=true;
-                who=2;
+                gameOver = true;
+                gameWon = true;
+                who = 2;
                 continue;
             }
-            if(filled())
-            gameOver=true;
+            if (filled())
+                gameOver = true;
         }
-        if(gameWon)
-        cout<<"Player "<<who<<" Won"<<" Congragulations!"<<endl;
+        if (gameWon)
+            cout << "Player " << who << " Won"
+                 << " Congratulations!" << endl;
         else
-        cout<<"Game Tied"<<endl;
-
+            cout << "Game Tied" << endl;
+    }
+    void clearScreenWindows()
+    {
+        system("cls"); // Clears the terminal screen on Windows
+    }
+    void clearScreenUnix()
+    {
+        system("clear"); // Clears the terminal screen on Unix/Linux
     }
 
-    bool valid(int row ,int col)
+    void clearScreen()
     {
-        return arr[row][col]==0;
+        #ifdef _WIN32
+        clearScreenWindows();
+        #else
+        clearScreenUnix();
+        #endif
+        printBoard();
+    }
+    bool valid(int row, int col)
+    {
+        return arr[row][col] == 0;
     }
 
     void printBoard()
     {
-        for(int i=0;i<3;i++){
-            cout<<"|";
-        for(int j=0;j<3;j++)
+        for (int i = 0; i < 3; i++)
         {
-            if(arr[i][j]==0)
+            cout << "|";
+            for (int j = 0; j < 3; j++)
             {
-                cout<<"___|";
+                if (arr[i][j] == 0)
+                {
+                    cout << "___|";
+                }
+                else if (arr[i][j] == 'O')
+                {
+                    cout << " O |";
+                }
+                else
+                {
+                    cout << " X |";
+                }
             }
-            else if(arr[i][j]=='O')
-            {
-                cout<<" O |";
-            }
-            else{
-                cout<<" X |";
-            }
-        }
-        cout<<endl<<endl;
+            cout << endl
+                 << endl;
         }
     }
     bool filled()
     {
-        for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
-        {
-            if(arr[i][j]==0)
-            return false;
-        }
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+            {
+                if (arr[i][j] == 0)
+                    return false;
+            }
         return true;
     }
     void player1Input()
     {
-        int row,col;
-        player1Back:cout<<"\n Player 1 Moves"<<endl;
-        cout<<"\n Enter Your Move\n"<<"ROW:";
-        cin>>row;
-        cout<<"\nCOLUMN:";
-        cin>>col;
+        int row, col, temp;
+    player1Back:
+        clearScreen();
+        cout << "\n Player 1 Moves" << endl;
+        cout << "\n Enter Your Move\n"
+             << " ";
+        cin >> temp;
 
-        if((row<=3 && row>=1 ) && (col>=1 && col<=3) && valid(row-1,col-1))
+        --temp;
+        row = temp / 3+1;
+        col = temp % 3+1;
+        
+        cout<<" "<<row<<" "<<col<<endl;
+
+        if ((row <= 3 && row >= 1) && (col >= 1 && col <= 3) && valid(row - 1, col - 1))
         {
-            arr[row-1][col-1]='O';
+            arr[row - 1][col - 1] = 'O';
             printBoard();
         }
-        else{
-            cout<<"Invalid Input\n";
+        else
+        {
+            cout << "Invalid Input\n";
             goto player1Back;
         }
     }
@@ -109,39 +137,46 @@ class Game{
     bool checkWin(int player)
     {
         int which;
-        if(player==1)
-        which='O';
+        if (player == 1)
+            which = 'O';
         else
-        which='X';
+            which = 'X';
+        // I was lzy to find the logic for the loop
         return (
-            (arr[0][0]==arr[1][1] and arr[1][1]==arr[2][2] and arr[1][1]==which)||
-            (arr[1][1]==arr[0][3] and arr[1][1]==arr[2][0] and arr[1][1]==which)||
+            (arr[0][0] == arr[1][1] and arr[1][1] == arr[2][2] and arr[1][1] == which) ||
+            (arr[1][1] == arr[0][3] and arr[1][1] == arr[2][0] and arr[1][1] == which) ||
 
-            (arr[0][0]==arr[0][1] and arr[0][1]==arr[0][2] and arr[0][1]==which)||
-            (arr[1][0]==arr[1][1] and arr[1][1]==arr[0][2] and arr[1][1]==which)||
-            (arr[2][0]==arr[2][1] and arr[2][1]==arr[2][2] and arr[2][1]==which)||
+            (arr[0][0] == arr[0][1] and arr[0][1] == arr[0][2] and arr[0][1] == which) ||
+            (arr[1][0] == arr[1][1] and arr[1][1] == arr[0][2] and arr[1][1] == which) ||
+            (arr[2][0] == arr[2][1] and arr[2][1] == arr[2][2] and arr[2][1] == which) ||
 
-            (arr[0][0]==arr[1][0] and arr[1][0]==arr[2][0] and arr[0][0]==which)||
-            (arr[0][1]==arr[1][1] and arr[1][1]==arr[2][1] and arr[2][1]==which)||
-            (arr[0][2]==arr[1][2] and arr[1][2]==arr[2][2] and arr[1][2]==which));
+            (arr[0][0] == arr[1][0] and arr[1][0] == arr[2][0] and arr[0][0] == which) ||
+            (arr[0][1] == arr[1][1] and arr[1][1] == arr[2][1] and arr[2][1] == which) ||
+            (arr[0][2] == arr[1][2] and arr[1][2] == arr[2][2] and arr[1][2] == which));
     }
 
     void player2Input()
     {
-        int row,col;
-        player2Back:cout<<"\n Player 2 Moves"<<endl;
-        cout<<"\n Enter Your Move\n"<<"ROW:";
-        cin>>row;
-        cout<<"\nCOLUMN:";
-        cin>>col;
+        int row, col, temp;
+    player2Back:
+        clearScreen();
+        cout << "\n Player 2 Moves" << endl;
+        cout << "\n Enter Your Move\n"
+             << " ";
+        cin >> temp;
 
-        if((row<=3 && row>=1 ) && (col>=1 && col<=3) && valid(row-1,col-1))
+        --temp;
+        row = temp / 3 +1;
+        col = temp % 3 +1;
+
+        if ((row <= 3 && row >= 1) && (col >= 1 && col <= 3) && valid(row - 1, col - 1))
         {
-            arr[row-1][col-1]='X';
+            arr[row - 1][col - 1] = 'X';
             printBoard();
         }
-        else{
-            cout<<"Invalid Input\n";
+        else
+        {
+            cout << "Invalid Input\n";
             goto player2Back;
         }
     }
